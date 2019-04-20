7f3ac875267cdb5cb3eda5089704179bbc4cd56c
Jack Rose <jackderryrose@gmail.com>
2019-04-20 12:01:21 +1000

    update display scripts to restart compton

---
+++ .screenlayout/cc.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output DP-2-1 --mode 1920x1200 --pos 1920x0 --rotate normal --output DP-2-2 --off --output DP-2-3 --off --output eDP-1 --primary --mode 1920x1080 --pos 960x1200 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-2 --off --output DP-1 --mode 1920x1200 --pos 0x0 --rotate normal
+killall -USR1 compton
