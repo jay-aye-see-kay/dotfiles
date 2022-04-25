c9beb28d046e6498d9503da3848eddce509990a4
Jack Rose <user@jackrose.co.nz>
2022-04-26 09:03:56 +1000

    vim: update nvim-cmp, putting default keymaps back
    - they were removed in a recent version

--- main/.config/nvim/init.lua
+++ main/.config/nvim/init.lua
@@ -326,17 +326,14 @@ cmp.setup({
 			require("luasnip").lsp_expand(args.body)
 		end,
 	},
-	mapping = {
+	mapping = cmp.mapping.preset.insert({
 		["<C-y>"] = cmp.mapping.confirm({ select = true }),
 		["<C-f>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
 		["<C-e>"] = cmp.mapping({
 			i = cmp.mapping.abort(),
 			c = cmp.mapping.close(),
 		}),
-		-- ['<CR>'] = cmp.mapping.confirm({ select = true }),
-		-- ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-		-- ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-	},
+	}),
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp" },
 		{ name = "luasnip" },
